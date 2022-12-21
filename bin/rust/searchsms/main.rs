55a686e0eb8396cd2add2b20d2953a7065586c81
Andrew Gallant <jamslam@gmail.com>
2022-12-21 13:40:02 -0500

    bin/rust: add program to search through SMS backups
    It searches through this crazy XML dump (gigabytes big) for text
    messages. It doesn't extract every bit of data (particularly media), but
    does pull out all of the plain text.
    
    Normally I'd try to just do this sort of thing ad hoc with grep and awk,
    but holy hell XML is too annoying to deal with that way. And the data is
    just so big that it really helps to have an easy to use presentation
    style.

---
+++ bin/rust/searchsms/main.rs
@@ -0,0 +1,437 @@
+#![allow(warnings)]
+
+use std::path::{Path, PathBuf};
+
+use anyhow::Context;
+use chrono::{DateTime, FixedOffset, Local, NaiveDateTime, TimeZone};
+use regex::Regex;
+use roxmltree::{Document, Node};
+
+fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let config = Config::parse(std::env::args_os())?;
+    let mut raws: Vec<(&Path, String)> = vec![];
+    for path in config.paths.iter() {
+        log::trace!("reading {}", path.display());
+        let raw = std::fs::read_to_string(path)
+            .with_context(|| format!("{}", path.display()))?;
+        raws.push((path, raw));
+    }
+    let mut docs = vec![];
+    for (path, raw) in raws.iter() {
+        log::trace!("parsing {}", path.display());
+        let doc = Document::parse(raw)
+            .with_context(|| format!("{}", path.display()))?;
+        docs.push(Doc { path: path.to_path_buf(), doc });
+    }
+    let mut msgs = vec![];
+    for doc in docs.iter() {
+        log::trace!("searching {}", doc.path.display());
+        let smses = doc.find_smses()?;
+        for c in smses.children() {
+            let Some(msg) = doc.extract(c)? else { continue };
+            if let Some(ref re) = config.body_regex {
+                if !re.is_match(&msg.body) {
+                    continue;
+                }
+            }
+            if let Some(ref re) = config.contact_regex {
+                if !msg.contacts.iter().any(|c| re.is_match(c)) {
+                    continue;
+                }
+            }
+            msgs.push(msg);
+        }
+    }
+    if config.reverse {
+        msgs.sort_by(|m1, m2| m1.date.cmp(&m2.date).reverse());
+    } else {
+        msgs.sort_by(|m1, m2| m1.date.cmp(&m2.date));
+    }
+    output_human(&msgs, &mut std::io::stdout())?;
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Message {
+    /// Whether the message was sent by me or received by me.
+    kind: MessageKind,
+    /// The literal timestamp string associated with this message. This is
+    /// useful as an ID of sorts for error reporting.
+    timestamp: String,
+    /// All of the contact names found in the message. This is the result of
+    /// splitting the 'contact_name' string by comma.
+    contacts: Vec<String>,
+    /// All of the addresses found in the message.
+    addresses: Vec<String>,
+    /// The actual body of the message. For mms messages, this corresponds
+    /// to the part that is 'text/plain'.
+    body: String,
+    /// A date associated with the message. It corresponds to the 'date'
+    /// attribute and is in units of milliseconds. It is *probably* the
+    /// "received datetime." It always appears to be in UTC.
+    date: DateTime<Local>,
+    /// A readable date string devoid of timezone info. It's probably just
+    /// "naive local time."
+    date_readable: Option<NaiveDateTime>,
+    /// From what I can tell, this is equivalent to 'date', but with
+    /// milliseconds always set to `0`. (Its units is still milliseconds for
+    /// 'sms' but apparently seconds for 'mms'. It appears to always be in
+    /// UTC.
+    date_sent: Option<DateTime<Local>>,
+}
+
+#[derive(Clone, Copy, Debug)]
+enum MessageKind {
+    Sent,
+    Recv,
+}
+
+#[derive(Debug)]
+struct Doc<'i> {
+    path: PathBuf,
+    doc: Document<'i>,
+}
+
+impl<'i> Doc<'i> {
+    fn find_smses<'a>(&'a self) -> anyhow::Result<Node<'a, 'i>> {
+        for c in self.doc.root().children() {
+            if c.tag_name().name() == "smses" {
+                return Ok(c);
+            }
+        }
+        anyhow::bail!(
+            "{}: could not find first child of root",
+            self.path.display()
+        )
+    }
+
+    fn extract<'a>(
+        &'a self,
+        node: Node<'a, 'i>,
+    ) -> anyhow::Result<Option<Message>> {
+        if !node.is_element() {
+            return Ok(None);
+        }
+        let msg = match node.tag_name().name() {
+            // All SMS are just plain text, so we always get a message.
+            "sms" => {
+                let Some(timestamp) = node.attribute("date") else {
+                    anyhow::bail!("failed to find timestamp for <sms> node")
+                };
+                Some(
+                    self.extract_from_sms(node, timestamp)
+                        .with_context(|| format!("{}", self.path.display()))
+                        .with_context(|| {
+                            format!("timestamp of <sms> node: {}", timestamp)
+                        })?,
+                )
+            }
+            // ... but an MMS might just be an image, so we might not get an
+            // actual plain text message.
+            "mms" => {
+                let Some(timestamp) = node.attribute("date") else {
+                    anyhow::bail!("failed to find timestamp for <mms> node")
+                };
+                self.extract_from_mms(node, timestamp)
+                    .with_context(|| format!("{}", self.path.display()))
+                    .with_context(|| {
+                        format!("timestamp of <mms> node: {}", timestamp)
+                    })?
+            }
+            unknown => anyhow::bail!(
+                "{}: unknown child of 'smses': {}",
+                self.path.display(),
+                unknown
+            ),
+        };
+        Ok(msg)
+    }
+
+    fn extract_from_sms<'a>(
+        &'a self,
+        node: Node<'a, 'i>,
+        timestamp: &str,
+    ) -> anyhow::Result<Message> {
+        let mut kind = None;
+        let mut contacts = vec![];
+        let mut addresses = vec![];
+        let mut body = None;
+        let mut date = None;
+        let mut date_readable = None;
+        let mut date_sent = None;
+        for a in node.attributes() {
+            if a.name() == "type" {
+                match a.value() {
+                    "1" => kind = Some(MessageKind::Recv),
+                    "2" => kind = Some(MessageKind::Sent),
+                    unknown => {
+                        anyhow::bail!("unknown SMS type '{:?}'", unknown)
+                    }
+                }
+            } else if a.name() == "contact_name" {
+                // Since this is just SMS, the contact name should always be
+                // singular. I think...
+                contacts.push(a.value().to_string());
+            } else if a.name() == "address" {
+                // Same as for contacts. For SMS, there should only be one.
+                addresses.push(a.value().to_string());
+            } else if a.name() == "body" {
+                body = Some(a.value().to_string());
+            } else if a.name() == "date" {
+                date = Some(
+                    parse_utc_timestamp_millis(a.value())
+                        .context("'date' attribute")?,
+                );
+            } else if a.name() == "readable_date" {
+                date_readable = Some(
+                    parse_readable_date(a.value())
+                        .context("'readable_date attribute")?,
+                );
+            } else if a.name() == "date_sent" && a.value() != "0" {
+                date_sent = Some(
+                    parse_utc_timestamp_millis(a.value())
+                        .context("'date_sent' attribute")?,
+                );
+            }
+        }
+        let Some(kind) = kind else {
+            anyhow::bail!("failed to find SMS message kind")
+        };
+        if contacts.is_empty() {
+            anyhow::bail!("failed to find SMS message contacts")
+        }
+        let Some(body) = body else {
+            anyhow::bail!("failed to find SMS message body")
+        };
+        let Some(date) = date else {
+            anyhow::bail!("failed to find date for message")
+        };
+        Ok(Message {
+            kind,
+            timestamp: timestamp.to_string(),
+            contacts,
+            addresses,
+            body,
+            date,
+            date_readable,
+            date_sent,
+        })
+    }
+
+    fn extract_from_mms<'a>(
+        &'a self,
+        node: Node<'a, 'i>,
+        timestamp: &str,
+    ) -> anyhow::Result<Option<Message>> {
+        let Some(body) = self.extract_body_from_mms(node)? else {
+            return Ok(None);
+        };
+        let mut kind = None;
+        let mut contacts = vec![];
+        let mut addresses = vec![];
+        let mut date = None;
+        let mut date_readable = None;
+        let mut date_sent = None;
+        for a in node.attributes() {
+            if a.name() == "m_type" {
+                match a.value() {
+                    "132" => kind = Some(MessageKind::Recv),
+                    "128" => kind = Some(MessageKind::Sent),
+                    unknown => {
+                        anyhow::bail!("unknown SMS type '{:?}'", unknown)
+                    }
+                }
+            } else if a.name() == "contact_name" {
+                contacts.extend(
+                    a.value().split(",").map(|s| s.trim().to_string()),
+                );
+            } else if a.name() == "address" {
+                addresses.extend(
+                    a.value().split("~").map(|s| s.trim().to_string()),
+                );
+            } else if a.name() == "date" {
+                date = Some(
+                    parse_utc_timestamp_millis(a.value())
+                        .context("'date' attribute")?,
+                );
+            } else if a.name() == "readable_date" {
+                date_readable = Some(
+                    parse_readable_date(a.value())
+                        .context("'readable_date attribute")?,
+                );
+            } else if a.name() == "date_sent" && a.value() != "0" {
+                date_sent = Some(
+                    parse_utc_timestamp_seconds(a.value())
+                        .context("'date_sent' attribute")?,
+                );
+            }
+        }
+        let Some(kind) = kind else {
+            anyhow::bail!("failed to find SMS message kind")
+        };
+        if contacts.is_empty() {
+            anyhow::bail!("failed to find SMS message contacts")
+        }
+        let Some(date) = date else {
+            anyhow::bail!("failed to find date for message")
+        };
+        Ok(Some(Message {
+            kind,
+            timestamp: timestamp.to_string(),
+            contacts,
+            addresses,
+            body,
+            date,
+            date_readable,
+            date_sent,
+        }))
+    }
+
+    fn extract_body_from_mms<'a>(
+        &'a self,
+        node: Node<'a, 'i>,
+    ) -> anyhow::Result<Option<String>> {
+        let mut body: Option<String> = None;
+        for c in node.descendants() {
+            if c.tag_name().name() != "part" {
+                continue;
+            }
+            let Some(content_type) = c.attribute("ct") else { continue };
+            if content_type != "text/plain" {
+                continue;
+            }
+            let Some(text) = c.attribute("text") else {
+                anyhow::bail!("'text/plain' part node has no 'text' attr")
+            };
+            if let Some(ref mut body) = body {
+                body.push_str(" ... ");
+                body.push_str(text);
+            } else {
+                body = Some(text.to_string());
+            }
+        }
+        Ok(body)
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+struct Config {
+    body_regex: Option<Regex>,
+    contact_regex: Option<Regex>,
+    paths: Vec<PathBuf>,
+    reverse: bool,
+}
+
+impl Config {
+    /// Parse the given OS string args into a `window-grep`
+    /// configuration.
+    fn parse<I>(args: I) -> anyhow::Result<Config>
+    where
+        I: IntoIterator<Item = std::ffi::OsString> + 'static,
+    {
+        use lexopt::{Arg::*, ValueExt};
+
+        const USAGE: &str = "
+Usage: searchsms [options] <body-regex> <path> ...
+
+Options:
+  -c, --contact <REGEX>
+    Only show text messages with a contact name matching the given regex.
+
+  -r, --reverse
+    Sort in reverse chronological order.
+";
+
+        let mut config = Config::default();
+        let mut parser = lexopt::Parser::from_iter(args);
+        while let Some(arg) = parser.next()? {
+            match arg {
+                Short('h') | Long("help") => {
+                    anyhow::bail!(USAGE);
+                }
+                Short('c') | Long("contact") => {
+                    let pat = parser.value()?.parse::<String>()?;
+                    config.contact_regex = Some(Regex::new(&pat)?);
+                }
+                Short('r') | Long("reverse") => {
+                    config.reverse = true;
+                }
+                Value(v) => {
+                    if config.body_regex.is_none() {
+                        let pat = v.parse::<String>()?;
+                        config.body_regex = Some(Regex::new(&pat)?);
+                    } else {
+                        config.paths.push(PathBuf::from(v));
+                    }
+                }
+                _ => return Err(arg.unexpected().into()),
+            }
+        }
+        anyhow::ensure!(
+            config.body_regex.is_some(),
+            "must provide regex to search body of SMS",
+        );
+        anyhow::ensure!(
+            !config.paths.is_empty(),
+            "must provide at least one path"
+        );
+        Ok(config)
+    }
+}
+
+fn output_human(
+    msgs: &[Message],
+    mut wtr: impl std::io::Write,
+) -> anyhow::Result<()> {
+    for (i, msg) in msgs.iter().enumerate() {
+        if i > 0 {
+            writeln!(wtr, "{}", "-".repeat(79))?;
+        }
+        match msg.kind {
+            MessageKind::Sent => write!(wtr, "   TO: ")?,
+            MessageKind::Recv => write!(wtr, " FROM: ")?,
+        }
+        writeln!(wtr, "{}", msg.contacts.join(", "))?;
+        writeln!(wtr, "PHONE: {}", msg.addresses.join(", "))?;
+        writeln!(wtr, " DATE: {}", msg.date.format("%Y-%m-%d %H:%M:%S"))?;
+        writeln!(wtr, "")?;
+        writeln!(wtr, "{}", textwrap::wrap(&msg.body, 79).join("\n"))?;
+    }
+    Ok(())
+}
+
+fn parse_utc_timestamp_seconds(
+    value: &str,
+) -> anyhow::Result<DateTime<Local>> {
+    let seconds = value
+        .parse::<i64>()
+        .context("failed to parse timestamp as seconds")?;
+    let Some(millis) = seconds.checked_mul(1000) else {
+        anyhow::bail!("seconds overflowed when converting to milliseconds")
+    };
+    utc_datetime_from_timestamp_millis(millis)
+}
+
+fn parse_utc_timestamp_millis(value: &str) -> anyhow::Result<DateTime<Local>> {
+    let millis = value
+        .parse::<i64>()
+        .context("failed to parse timestamp as milliseconds")?;
+    utc_datetime_from_timestamp_millis(millis)
+}
+
+fn utc_datetime_from_timestamp_millis(
+    millis: i64,
+) -> anyhow::Result<DateTime<Local>> {
+    let Some(ndt) = NaiveDateTime::from_timestamp_millis(millis) else {
+        anyhow::bail!("invalid timestamp")
+    };
+    Ok(Local.from_utc_datetime(&ndt))
+}
+
+fn parse_readable_date(value: &str) -> anyhow::Result<NaiveDateTime> {
+    // Example: Apr 1, 2022 20:46:15
+    NaiveDateTime::parse_from_str(value, "%h %-d, %Y %H:%M:%S")
+        .context("failed to parse readable date")
+}
